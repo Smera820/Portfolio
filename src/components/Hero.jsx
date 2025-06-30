2bb4013b325cfd5062d75064d7357333762d915a
smerasabu <smerasabu@gmail.com>
2025-06-30 18:30:00 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,13 @@ function Hero() {
         <div className='min-h-screen overflow-y-auto pb-9 lg:mb-36 pl-8'>
             <div className='flex flex-wrap lg:flex-row-reverse'>
                 <div className='w-full lg:w-1/2'>
-                    <div className='flex justify-center lg:p-8'>
+                    <div className='flex justify-center lg:p-4'>
                         <img src={pic} alt="profile" className='max-w-xs max-h-69 rounded-full object-cover border-2 border-stone-900' />
                     </div>
                 </div>
                 <div className='w-full lg:w-1/2'>
                     <div className='flex flex-col items-center lg:items-start mt-10'>
-                        <h2 className='pb-2 text-3xl tracking-tighter lg:text-8xl'>
+                        <h2 className='pb-2 text-3xl tracking-tighter lg:text-6xl'>
                             Smera Sabu
                         </h2>
                         <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-2xl tracking-tight text-center text-transparent'>
@@ -19,12 +19,12 @@ function Hero() {
                         </span>
                         <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
 
-                            Aspiring Full Stack Developer skilled in MERN Stack, building responsive UIs, and developing
-                            clean, scalable RESTful APIs.  Eager to contribute to dynamic teams and grow in a
-                            collaborative development environment
+                            Passionate and dedicated Full Stack Developer with hands-on experience in building responsive web applications using the MERN stack. Skilled in developing both frontend and backend, with a strong focus on clean code and user-friendly interfaces. 
+                            Eager to join a dynamic team where I can contribute to real-world projects and continue growing as a developer.
                         </p>
-                        <a href="https://drive.google.com/file/d/1Iq00xfDcmuwsWs6isjRxYcvAk3KPxvFO/view?usp=sharing" className='bg-white text-black rounded-full shadow px-6 py-3  '>
-                        Download Resume</a>
+                        <a href="https://drive.google.com/file/d/1Iq00xfDcmuwsWs6isjRxYcvAk3KPxvFO/view?usp=sharing" 
+                        className='bg-white text-black rounded-full shadow px-6 py-3 hover:scale-105 transition-transform duration-300 '>
+                            View Resume</a>
                     </div>
 
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import logo from '../assets/logo.webp'
+import logo from '../assets/s.png'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 function Navbar() {
   return (
     <nav className='flex items-center justify-between p-6'>
 
-      <div className='flex flex-shrink-0 items-center'>
-        <a href="/" aria-label='Home'><img src={logo} className='mx-2' width={50} height={33} /></a>
+      <div className='flex flex-shrink-0 items-center '>
+        <a href="/" aria-label='Home'><img src={logo} className='mx-2 w-20 h-20' /></a>
       </div>
 
       <div className='m-8 flex items-center justify-center gap-6 text-2xl'>
